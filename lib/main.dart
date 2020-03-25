14db54c2d6532e6a63e148824e5504e9a23eb4f4
Pedro Satin <pedro5satin@gmail.com>
2020-03-24 23:46:08 -0300

    Logica e ajustes de layout

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,49 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  TextEditingController weightController;
+  TextEditingController heightController;
+
+  String _message = "Informe os valores";
+
+  void _resetFields() {
+    weightController.text = "";
+    heightController.text = "";
+    setState(() {
+      _message = "Informe os valores";
+    });
+  }
+
+  void _calculateImc() {
+    setState(() {
+      double weight = double.parse(weightController.text);
+      double height = double.parse(heightController.text) / 100;
+
+      double imc = weight / (height * height);
+
+      if (imc < 18.6) {
+        _message = "Você está abaixo do peso, IMC: ${imc.toStringAsPrecision(4)}";
+      }
+      if (imc >= 18.6 && imc < 24.9) {
+        _message = "Peso ideal, IMC: ${imc.toString()}";
+      }
+      if (imc >= 24.9 && imc < 29.9) {
+        _message = "Levemente acima do peso, IMC: ${imc.toString()}";
+      }
+      if (imc >= 29.9 && imc < 34.9) {
+        _message = "Obesidade grau I, IMC: ${imc.toString()}";
+      }
+      if (imc >= 34.9 && imc < 39.9) {
+        _message = "Obesidade grau II, IMC: ${imc.toString()}";
+      }
+      if (imc >= 40) {
+        _message = "Obesidade grau III, IMC: ${imc.toString()}";
+      }
+
+    });
+    
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,12 +63,13 @@ class _HomeState extends State<Home> {
           actions: <Widget>[
             IconButton(
               icon: Icon(Icons.refresh),
-              onPressed: () => {},
+              onPressed: _resetFields ,
             )
           ],
         ),
         backgroundColor: Colors.white,
         body: SingleChildScrollView(
+          padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
@@ -42,27 +86,36 @@ class _HomeState extends State<Home> {
                 ),
                 textAlign: TextAlign.center,
                 style: TextStyle(color: Colors.green, fontSize: 25),
+                controller: weightController,
               ),
-              TextField(
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(
-                  labelText: "Informe sua altura (cm): ",
-                  labelStyle: TextStyle(color: Colors.green),
-                ),
-                textAlign: TextAlign.center,
-                style: TextStyle(color: Colors.green, fontSize: 25),
-              ),
-              Container(
-                height: 50,
-                child: RaisedButton(
-                  onPressed: () => {},
-                  child: Text("Calcular",
-                      style: TextStyle(color: Colors.white, fontSize: 25)),
-                  color: Colors.green,
+              Padding(
+                  padding: EdgeInsets.fromLTRB(0, 5, 0, 0),
+                  child: TextField(
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                      labelText: "Informe sua altura (cm): ",
+                      labelStyle: TextStyle(color: Colors.green),
+                    ),
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.green, fontSize: 25),
+                    controller: heightController,
+                  )),
+              Padding(
+                padding: EdgeInsets.fromLTRB(0, 20, 0, 20),
+                child: Container(
+                  height: 50,
+                  child: RaisedButton(
+                    onPressed: _calculateImc,
+                    child: Text("Calcular",
+                        style: TextStyle(color: Colors.white, fontSize: 25)),
+                    color: Colors.green,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15)),
+                  ),
                 ),
               ),
               Text(
-                "",
+                _message,
                 textAlign: TextAlign.center,
                 style: TextStyle(color: Colors.green, fontSize: 25),
               )
